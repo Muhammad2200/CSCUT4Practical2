fd259523c8805730a604f9a39f95b4132065c246
Muhammad2200 <129514473+Muhammad2200@users.noreply.github.com>
2023-04-08 02:57:13 +0400

    Add files via upload

---
+++ FormatNames.java
@@ -0,0 +1,15 @@
+package Files;
+
+import java.io.File;
+
+import java.util.Scanner;
+
+public class FormatNames {
+    public static void main(String[] args) {
+        // Set input and output file names
+        String inputFile = "input.txt";
+        String outputFile = "output.txt"; 
+            }
+}
+}
+}
